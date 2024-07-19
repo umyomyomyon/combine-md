5c96c147582034e1939f28555820606ae738c8c2
umyomyomyon <minecraft.sentry.yym@gmail.com>
2024-07-19 11:52:03 +0900

    add args repository_name

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_mdx_files(directory):
     mdx_files = []
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.mdx'):
+            if file.endswith('.mdx') or file.endswith('.md'):
                 mdx_files.append(os.path.join(root, file))
     return mdx_files
 
@@ -22,29 +22,30 @@ def make_separator(path, keyword):
         # キーワードが見つからなかった場合は元のパスを返す
         return path
 
-def concatenate_mdx_files(mdx_files, output_file):
+def concatenate_mdx_files(mdx_files, output_file, repo_name):
     """複数の.mdxファイルを結合して一つの大きな.mdxファイルを作成する"""
     with open(output_file, 'w', encoding='utf-8') as outfile:
         for mdx_file in mdx_files:
-            separator = make_separator(os.path.relpath(mdx_file), 'next.js')
+            separator = make_separator(os.path.relpath(mdx_file), repo_name)
             outfile.write(f"----------{separator}--------\n")
             with open(mdx_file, 'r', encoding='utf-8') as infile:
                 outfile.write(infile.read())
                 outfile.write("\n\n")  # Ensure spacing between files
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <directory>")
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <directory> <repository_name>\nexample: python main.py docs my-repo")
         sys.exit(1)
 
     docs_directory = sys.argv[1]
     if not os.path.isdir(docs_directory):
         print(f"The directory {docs_directory} does not exist or is not a directory.")
         sys.exit(1)
+    repository_name = sys.argv[2]
     
-    output_file = 'output/combined.md'
+    output_file = f'output/{repository_name}-combined.md'
     mdx_files = get_mdx_files(docs_directory)
-    concatenate_mdx_files(mdx_files, output_file)
+    concatenate_mdx_files(mdx_files, output_file, repository_name)
 
 if __name__ == '__main__':
     main()
